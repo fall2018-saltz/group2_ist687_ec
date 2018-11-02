860f6e017b7b3cadc281d1a05c9ef8c1767cd809
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 20:16:32 -0400

    Saving ChangeColumnNames.R

--- ChangeColumnNames.R
+++ ChangeColumnNames.R
@@ -1,4 +1,5 @@
 
+library(data.table)
 setnames(satisfactionSurvey, old=c('X..of.Flight.with.other.Airlines'), new = c('Percentage of Flight with other Airlines'))
 setnames(satisfactionSurvey, old=c('No.of.Flights.p.a.', 'Percentage of Flight with other Airlines'), new = c('No.of.Flights.per.customer','Percentage.of.Flight.with.other.Airlines'))
 setnames(satisfactionSurvey, old=c('Orgin.City','No..of.other.Loyalty.Cards'), new = c('Origin.City','No.of.other.Loyalty.Cards'))
