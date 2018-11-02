a0e824f946f87d757c230e82bf8f9041fa5c9ba0
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 20:22:01 -0400

    removing clean3.R

--- clean3.R
+++
@@ -1,4 +0,0 @@
-
-index<-which(!(satisfactionSurvey$Satisfaction %in%c("1","2","3","4","5")))
-satisfactionSurvey<-satisfactionSurvey[-index,]
-satisfactionSurvey$Satisfaction <- factor(c("1","2","3","4","5"))
