660e406f649018b6f7954324fba6e1e8378c9192
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 20:57:05 -0400

    Saving RemoveUnnecessaryValues.R

--- RemoveUnnecessaryValues.R
+++ RemoveUnnecessaryValues.R
@@ -2,4 +2,5 @@
 index<-which(!(satisfactionSurvey$Satisfaction %in%c("1","2","3","4","5")))
 satisfactionSurvey<-satisfactionSurvey[-index,]
 satisfactionSurvey$Satisfaction <- factor(c("1","2","3","4","5"))
+ss<-satisfactionSurvey
 
