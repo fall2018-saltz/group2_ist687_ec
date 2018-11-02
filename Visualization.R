8a1b2e3e84e550034fcc3d132d6a848e59a9930d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 22:29:23 -0400

    Saving Visualization.R

--- Visualization.R
+++ Visualization.R
@@ -2,3 +2,5 @@
 library(ggplot2)
 
 plot1<-ggplot(ss2,aes(x=Age,y=Satisfcation))+geom_point(fill="Gender")
+plot1<-ggplot(ss2,aes(x=Age,y=Satisfcation))+geom_point(fill="Gender")
+plot1<-ggplot(ss2,aes(x=Age,y=Satisfcation))+geom_point(fill="Gender")
