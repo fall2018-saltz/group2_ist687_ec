0089bd6165f897ab9cd0ead51e96169b27c307e5
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 21:09:17 -0400

    Saving Visualization.R

--- Visualization.R
+++ Visualization.R
@@ -3,3 +3,5 @@ library(ggplot2)
 
 plot1<-ggplot(data=ss2,aes(x=Age,y=Satisfaction))+geom_point()
 plot1<-ggplot(data=ss2,aes(x=Age,y=Satisfaction))+geom_point()
+plot1<-ggplot(data=ss2,aes(x=Age,y=Satisfaction))+geom_point()
+plot1<-ggplot(data=ss2,aes(x=Age,y=Satisfaction))+geom_point()
