989f3710cc1b8adf85a7ce571774cf38e3c08571
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-14 14:27:00 -0500

    Saving lm.R

--- lm.R
+++ lm.R
@@ -2,7 +2,7 @@
 
 m<-lm(formula=as.numeric(Satisfaction)~Airline.Status+Age+Gender+Price.Sensitivity+No.of.Flights.per.annum+
 Percentage.of.Flight.with.other.Airlines+Type.of.Travel+ No.of.other.Loyalty.Cards+Shopping.Amount.at.Airport+
-Eating.and.Drinking.at.Airport+Class+Day.of.Month+Airline.Name+as.numeric(Origin.City)+as.numeric(Destination.City)+Scheduled.Departure.Hour
+Eating.and.Drinking.at.Airport+Class+Day.of.Month+Airline.Name+as.character(Origin.City)+as.character(Destination.City)+Scheduled.Departure.Hour
 ,data=ss_date,na.action=na.omit)
 summary(m)
 
