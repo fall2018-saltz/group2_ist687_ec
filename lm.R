4795024e1507db6f8cb254f0f1b0553e69e076f7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-14 14:45:01 -0500

    Saving lm.R

--- lm.R
+++ lm.R
@@ -3,7 +3,7 @@ levels(Flight.cancelled)
 m<-lm(formula=as.numeric(Satisfaction)~Airline.Status+Age+Gender+Price.Sensitivity+No.of.Flights.per.annum+
 Percentage.of.Flight.with.other.Airlines+Type.of.Travel+ No.of.other.Loyalty.Cards+Shopping.Amount.at.Airport+
 Eating.and.Drinking.at.Airport+Class+Day.of.Month+Airline.Name+Scheduled.Departure.Hour+Departure.Delay.in.Minutes+
-Arrival.Delay.in.Minutes+asFlight.cancelled)  
+Arrival.Delay.in.Minutes+Flight.cancelled)  
 ,data=ss_date,na.action=na.omit)
 summary(m)
 
