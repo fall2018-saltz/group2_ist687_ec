01ba305278e63799f532f309530d3ec5a11a3fb0
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-14 14:52:05 -0500

    Saving lm.R

--- lm.R
+++ lm.R
@@ -3,7 +3,7 @@ ss_date$Flight.cancelled<-factor(c("No","Yes"))
 m<-lm(formula=as.numeric(Satisfaction)~Airline.Status+Age+Gender+Price.Sensitivity+No.of.Flights.per.annum+
 Percentage.of.Flight.with.other.Airlines+Type.of.Travel+ No.of.other.Loyalty.Cards+Shopping.Amount.at.Airport+
 Eating.and.Drinking.at.Airport+Class+Day.of.Month+Airline.Name+Scheduled.Departure.Hour+Departure.Delay.in.Minutes+
-Arrival.Delay.in.Minutes+Flight.cancelled$ Flight.time.in.minutes+Flight.Distance+Arr.Delay.greater.5.Mins 
+Arrival.Delay.in.Minutes+Flight.cancelled+Flight.time.in.minutes+Flight.Distance+Arr.Delay.greater.5.Mins 
 ,data=ss_date,na.action=na.omit)
 summary(m)
 
