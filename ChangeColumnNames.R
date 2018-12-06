9958c312f2e077eb7dbea458cfabce263e96bc29
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 23:26:20 -0500

    Saving ChangeColumnNames.R

--- ChangeColumnNames.R
+++ ChangeColumnNames.R
@@ -8,9 +8,9 @@ library(data.table)
 #setnames(satisfactionSurvey1, old=c('No.of.Flights.per.customer'), new = c('No.of.Flights.per.annum'))
 #colSatSurvey <- setnames(satisfactionSurvey1, old=c('Arrival.Delay.greater.5.Mins'), new = c('Arr.Delay.greater.5.Mins'))
 
-setnames(satisfactionSurvey, old=c('X..of.Flight.with.other.Airlines'), new = c('Percentage of Flight with other Airlines'))
-setnames(satisfactionSurvey, old=c('Percentage of Flight with other Airlines'), new = c('Percentage.of.Flight.with.other.Airlines'))
-setnames(satisfactionSurvey, old=c('Orgin.City','No..of.other.Loyalty.Cards'), new = c('Origin.City','No.of.other.Loyalty.Cards'))
-setnames(satisfactionSurvey, old=c('Arrival.Delay.greater.5.Mins'), new = c('Arr.Delay.greater.5.Mins'))
+setnames(satisfactionSurvey1, old=c('X..of.Flight.with.other.Airlines'), new = c('Percentage of Flight with other Airlines'))
+setnames(satisfactionSurvey1, old=c('Percentage of Flight with other Airlines'), new = c('Percentage.of.Flight.with.other.Airlines'))
+setnames(satisfactionSurvey1, old=c('Orgin.City','No..of.other.Loyalty.Cards'), new = c('Origin.City','No.of.other.Loyalty.Cards'))
+setnames(satisfactionSurvey1, old=c('Arrival.Delay.greater.5.Mins'), new = c('Arr.Delay.greater.5.Mins'))
 
 colSatSurvey
