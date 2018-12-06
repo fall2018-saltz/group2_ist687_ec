a3d28763ca6f5209c0569d067c36f1b773f46b84
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 21:26:31 -0500

    Saving CleaningSatisfactionColumn.R

--- CleaningSatisfactionColumn.R
+++ CleaningSatisfactionColumn.R
@@ -1,10 +1,4 @@
 
-library(data.table)
-
-library(dplyr)
-
 index<-which(!(dateSatSurvey$Satisfaction %in% c(1,2,3,4,5)))
 dateSatSurvey<-dateSatSurvey[-index,]
 dateSatSurvey
-
-any(is.NA(dateSatSurvey)
