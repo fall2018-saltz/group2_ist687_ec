9947873a9a0f5855afa5cd7aea193d2281b40a11
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 20:05:19 -0500

    Saving RemovingSpaces.R

--- RemovingSpaces.R
+++ RemovingSpaces.R
@@ -1,1 +1,11 @@
 
+gsub(" ", "", satisfactionSurvey$Airline.Status, fixed = TRUE)
+gsub(" ", "", satisfactionSurvey$Gender, fixed = TRUE)
+trimws(satisfactionSurvey$Type.of.Travel)
+trimws(satisfactionSurvey$Class)
+trimws(satisfactionSurvey$Airline.Code)
+trimws(satisfactionSurvey$Airline.Name)
+trimws(satisfactionSurvey$Origin.City)
+trimws(satisfactionSurvey$Origin.State)
+trimws(satisfactionSurvey$Destination.City)
+trimws(satisfactionSurvey$Destination.State)
