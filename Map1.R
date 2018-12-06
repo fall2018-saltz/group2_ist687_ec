e1328347ddcce60f88801da1338e1860c19fc04b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-06 00:23:24 -0500

    Saving Map1.R

--- Map1.R
+++ Map1.R
@@ -4,7 +4,7 @@
 
 us<-map_data("state")
 
-map1<-ggplot(satisfactionSurveyM1, aes(map_id=originstate))
+map1<-ggplot(satisfactionSurveyM1, aes(map_id=Origin.State))
 map1<-map1+geom_map(map=us, aes(fill=Satisfaction))
 map1<-map1+expand_limits(x=us$long, y=us$lat)
 map1<-map1+coord_map()+ggtitle("USA states Satisfaction")
