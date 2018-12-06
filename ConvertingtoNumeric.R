33d73a0a33f901544a0fdfd8bf8476cfaa1ba9a0
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 20:34:34 -0500

    Saving ConvertingtoNumeric.R

--- ConvertingtoNumeric.R
+++ ConvertingtoNumeric.R
@@ -1,1 +1,16 @@
 
+satisfactionSurvey$Satisfaction <- as.numeric(as.character(satisfactionSurvey$Satisfaction))
+satisfactionSurvey$Price.Sensitivity <- as.numeric(as.character(satisfactionSurvey$Price.Sensitivity))
+satisfactionSurvey$Year.of.First.Flight <- as.numeric(as.character(satisfactionSurvey$Year.of.First.Flight))
+satisfactionSurvey$No.of.Flights.p.a. <- as.numeric(as.character(satisfactionSurvey$No.of.Flights.p.a.))
+satisfactionSurvey$Percentage.of.Flight.with.other.Airlines <- as.numeric(as.character(satisfactionSurvey$Percentage.of.Flight.with.other.Airlines))
+satisfactionSurvey$No.of.other.Loyalty.Cards <- as.numeric(as.character(satisfactionSurvey$No.of.other.Loyalty.Cards))
+satisfactionSurvey$Shopping.Amount.at.Airport <- as.numeric(as.character(satisfactionSurvey$Shopping.Amount.at.Airport))
+satisfactionSurvey$Eating.and.Drinking.at.Airport <- as.numeric(as.character(satisfactionSurvey$Eating.and.Drinking.at.Airport))
+satisfactionSurvey$Day.of.Month <- as.numeric(as.character(satisfactionSurvey$Day.of.Month))
+satisfactionSurvey$Scheduled.Departure.Hour <- as.numeric(as.character(satisfactionSurvey$Scheduled.Departure.Hour))
+satisfactionSurvey$Departure.Delay.in.Minutes <- as.numeric(as.character(satisfactionSurvey$Departure.Delay.in.Minutes))
+satisfactionSurvey$Arrival.Delay.in.Minutes <- as.numeric(as.character(satisfactionSurvey$Arrival.Delay.in.Minutes))
+satisfactionSurvey$Flight.time.in.minutes <- as.numeric(as.character(satisfactionSurvey$Flight.time.in.minutes))
+satisfactionSurvey$Flight.Distance <- as.numeric(as.character(satisfactionSurvey$Flight.Distance))
+satisfactionSurvey
