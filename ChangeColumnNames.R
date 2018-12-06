72b01acb977759b0b59c149f2597fbdf11b0f053
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 20:06:01 -0500

    Saving ChangeColumnNames.R

--- ChangeColumnNames.R
+++ ChangeColumnNames.R
@@ -4,3 +4,4 @@ setnames(satisfactionSurvey, old=c('X..of.Flight.with.other.Airlines'), new = c(
 setnames(satisfactionSurvey, old=c('Percentage of Flight with other Airlines'), new = c('Percentage.of.Flight.with.other.Airlines'))
 setnames(satisfactionSurvey, old=c('Orgin.City','No..of.other.Loyalty.Cards'), new = c('Origin.City','No.of.other.Loyalty.Cards'))
 setnames(satisfactionSurvey, old=c('Arrival.Delay.greater.5.Mins'), new = c('Arr.Delay.greater.5.Mins'))
+satisfactionSurvey
