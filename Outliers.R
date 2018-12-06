3e890201212e0a61c6403f33ad17e6e04661902d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 22:44:29 -0500

    Saving Outliers.R

--- Outliers.R
+++ Outliers.R
@@ -1,4 +1,4 @@
 
-satSurvey$Flight.time.in.minutes[is.na(satSurvey$Flight.time.in.minutes)] <- 9999
-satSurvey$Departure.Delay.in.Minutes[is.na(satSurvey$Departure.Delay.in.Minutes)] <- 9999
-satSurvey$Arrival.Delay.in.Minutes[is.na(satSurvey$Arrival.Delay.in.Minutes)] <- 9999
+satSurvey45$Flight.time.in.minutes[is.na(satSurvey45$Flight.time.in.minutes)] <- 9999
+satSurvey45$Departure.Delay.in.Minutes[is.na(satSurvey45$Departure.Delay.in.Minutes)] <- 9999
+satSurvey45$Arrival.Delay.in.Minutes[is.na(satSurvey45$Arrival.Delay.in.Minutes)] <- 9999
