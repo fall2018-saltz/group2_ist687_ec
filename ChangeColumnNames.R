6e99a04f2038128e293a33b1b923527e02622bbc
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-06 00:13:22 -0500

    Saving ChangeColumnNames.R

--- ChangeColumnNames.R
+++ ChangeColumnNames.R
@@ -8,6 +8,9 @@ library(data.table)
 #setnames(satisfactionSurvey1, old=c('No.of.Flights.per.customer'), new = c('No.of.Flights.per.annum'))
 #colSatSurvey <- setnames(satisfactionSurvey1, old=c('Arrival.Delay.greater.5.Mins'), new = c('Arr.Delay.greater.5.Mins'))
 
+satisfactionSurvey$originstate<-tolower(satisfactionSurvey$Origin.State)
+satisfactionSurvey$origincity<-tolower(satisfactionSurvey$Origin.City)
+
 setnames(satisfactionSurvey1, old=c('X..of.Flight.with.other.Airlines'), new = c('Percentage of Flight with other Airlines'))
 setnames(satisfactionSurvey1, old=c('Percentage of Flight with other Airlines'), new = c('Percentage.of.Flight.with.other.Airlines'))
 setnames(satisfactionSurvey1, old=c('Orgin.City','No..of.other.Loyalty.Cards'), new = c('Origin.City','No.of.other.Loyalty.Cards'))
