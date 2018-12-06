ced8175bba0a4f2f53c8f268961d66f5cf4c3394
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 20:28:12 -0500

    force updated content when merge failed

--- ChangeColumnNames.R
+++ ChangeColumnNames.R
@@ -1,7 +1,9 @@
 
+summary(satisfactionSurvey)
+str(satisfactionSurvey)
 library(data.table)
 setnames(satisfactionSurvey, old=c('X..of.Flight.with.other.Airlines'), new = c('Percentage of Flight with other Airlines'))
-setnames(satisfactionSurvey, old=c('Percentage of Flight with other Airlines'), new = c('Percentage.of.Flight.with.other.Airlines'))
+setnames(satisfactionSurvey, old=c('No.of.Flights.p.a.', 'Percentage of Flight with other Airlines'), new = c('No.of.Flights.per.customer','Percentage.of.Flight.with.other.Airlines'))
 setnames(satisfactionSurvey, old=c('Orgin.City','No..of.other.Loyalty.Cards'), new = c('Origin.City','No.of.other.Loyalty.Cards'))
+setnames(satisfactionSurvey, old=c('No.of.Flights.per.customer'), new = c('No.of.Flights.per.annum'))
 setnames(satisfactionSurvey, old=c('Arrival.Delay.greater.5.Mins'), new = c('Arr.Delay.greater.5.Mins'))
-satisfactionSurvey
