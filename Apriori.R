cdfed04f0df8f5f545ea411983118d5a58bb9c98
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-06 00:55:44 -0500

    Saving Apriori.R

--- Apriori.R
+++ Apriori.R
@@ -1,5 +1,5 @@
 
 library("arules")
-ruleset<-apriori(satisfactionSurveyF,parameter = list(support=0.28,confidence=0.55),appearance = list(default="lhs",rhs="Satisfaction High"))
+ruleset<-apriori(satisfactionSurveyF1,parameter = list(support=0.28,confidence=0.55),appearance = list(default="lhs",rhs="Satisfaction High"))
 
 #inspect(ruleset)
