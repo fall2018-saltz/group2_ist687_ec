2d4205a13f8c7142edec0b6af9bd606bb1facc40
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-06 00:21:15 -0500

    Saving Map1.R

--- Map1.R
+++ Map1.R
@@ -3,3 +3,8 @@ satisfactionSurvey$originstate<-tolower(satisfactionSurvey$Origin.State)
 satisfactionSurvey$origincity<-tolower(satisfactionSurvey$Origin.City)
 
 us<-map_data("state")
+
+map1<-ggplot(satisfactionSurvey, aes(map_id=originstate))
+map1<-map1+geom_map(map=us, aes(fill=Satisfaction))
+map1<-map1+expand_limits(x=us$long, y=us$lat)
+map1<-map1+coord_map()+ggtitle("USA states Satisfaction")
