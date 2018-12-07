a740d4c47d217c75c21d3b7ac64446cd200373e0
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-07 13:57:59 -0500

    Saving Maps.R

--- Maps.R
+++ Maps.R
@@ -1,3 +1,34 @@
 
 library("ggplot2")
 library("mapproj")
+
+US <- map_data("state")
+
+SatByAirlines <- data.frame(plotMaps %>%
+                              group_by(Airline.Name) %>%
+                              dplyr::summarize(AverageSatisfaction = mean(Satisfaction, na.rm=TRUE)))
+
+SatByOriStates <- data.frame(plotMaps %>%
+                               group_by(Origin.State) %>%
+                               dplyr::summarize(AverageSatisfaction = mean(Satisfaction, na.rm=TRUE)))
+
+SatByOriStates <- SatByOriStates[order(SatByOriStates$AverageSatisfaction),] 
+SatByOriStates$Origin.State <- tolower(SatByOriStates$Origin.State)
+
+SatByOriStatesMap <- ggplot(SatByOriStates, aes(map_id = Origin.State))+ 
+  geom_map(map = US, aes(fill = AverageSatisfaction))+ 
+  expand_limits(x=US$long, y=US$lat)+
+  coord_map() + ggtitle("Average Satisfaction for Origin States") + labs (x="Longitude", y="Latitude")
+
+#3
+SatByDesStates <- data.frame(plotMaps %>%
+                               group_by(Destination.State) %>%
+                               dplyr::summarize(AverageSatisfaction = mean(Satisfaction, na.rm=TRUE)))
+SatByDesStates <- SatByDesStates [order(SatByDesStates$AverageSatisfaction),] 
+SatByDesStates$Destination.State<- tolower(SatByDesStates$Destination.State)
+
+SatByDesStatesMap <- ggplot(SatByDesStates, aes(map_id = Destination.State))+ 
+  geom_map(map = US, aes(fill = AverageSatisfaction))+ 
+  expand_limits(x=US$long, y=US$lat)+
+  coord_map() + ggtitle("Average Satisfaction for Destination States") + labs (x="Longitude", y="Latitude")+
+scale_fill_gradient(high = "darkred",low = "white")
