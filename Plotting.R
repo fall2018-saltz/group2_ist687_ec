a871e0e6d8fb6dcb25785c8c44f091813ff09e08
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-07 13:31:05 -0500

    Saving Plotting.R

--- Plotting.R
+++ Plotting.R
@@ -1,4 +1,5 @@
 
+library("ggplot2")
 library("mapproj")
 library("mapdata")
 us<-map_data("state")
@@ -8,7 +9,7 @@ map1<-ggplot(satisfactionSurveyP, aes(map_id=originstate))
 map1<-map1+geom_map(map=us, aes(fill=Satisfaction))
 map1<-map1+expand_limits(x=us$long, y=us$lat)
 map1<-map1+coord_map()+ggtitle("USA states Satisfaction")
-map1
+
 
 satisfactionSurveyP$Destination.State <- tolower(satisfactionSurveyP$Destination.State)
 map2<-ggplot(satisfactionSurveyP, aes(map_id=Destination.State))
@@ -16,8 +17,8 @@ map2<-map2+geom_map(map=us, aes(fill=Satisfaction))
 map2<-map2+expand_limits(x=us$long, y=us$lat)
 map2<-map2+coord_map()+ggtitle("USA states Satisfaction")
 
-#Tavleen's
+
 name.box<-ggplot(satisfactionSurveyP,aes(x=satisfactionSurveyP$Airline.Name,y=satisfactionSurveyP$Satisfaction))
 name.box<-name.box+geom_boxplot()
 name.box<-name.box+theme(axis.text.x=element_text(angle=90,hjust=1))
-name.box
+
