0169ab7f9b783757ec54642d614d4adf714f4484
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-07 12:22:08 -0500

    Saving TrainingAndTestData.R

--- TrainingAndTestData.R
+++ TrainingAndTestData.R
@@ -1,1 +1,6 @@
 
+randIndex<-  sample(1:dim(satisfactionSurveyF2)[1])
+cutPoint2_3 <- floor(2*dim(satisfactionSurveyF2)[1]/3)
+trainData<- satisfactionSurveyF2[randIndex[1:cutPoint2_3],]
+testData<- satisfactionSurveyF2[randIndex[(cutPoint2_3+1):dim(satisfactionSurveyF2)[1]],]
+testData
