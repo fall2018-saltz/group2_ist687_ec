2fdbbb52357d2b39b93157369b33f6d31ed9101b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-07 14:47:12 -0500

    Saving LinearModelling.R

--- LinearModelling.R
+++ LinearModelling.R
@@ -2,7 +2,7 @@
 m<-lm(formula=Satisfaction~Age+Gender+Airline.Status+Price.Sensitivity+Year.of.First.Flight+
         No.of.Flights.p.a.+Percentage.of.Flight.with.other.Airlines+Type.of.Travel+
         No.of.other.Loyalty.Cards+Shopping.Amount.at.Airport+Eating.and.Drinking.at.Airport+Class+
-        Day.of.Month+Airline.Name+Scheduled.Departure.Hour+
+        Day.of.Month+Scheduled.Departure.Hour+
         Departure.Delay.in.Minutes+Arrival.Delay.in.Minutes+Flight.time.in.minutes+
         Flight.Distance,data=satisfactionSurvey)
         
