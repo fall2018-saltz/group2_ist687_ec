c7144528ed26c22f324f073224b4d40990440222
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-07 14:22:30 -0500

    Saving SupportVectorMachine.R

--- SupportVectorMachine.R
+++ SupportVectorMachine.R
@@ -10,10 +10,11 @@ svmOutput  <- ksvm(Satisfaction~ Age+Gender+Type.of.Travel+Flight.cancelled+Airl
 svmOutput 
 
 #Using the predict( ) function to validate the model against test data 
-svmPred <- predict(svmOutput, tesTData, type="votes")#Reviewing contents of svmPred using str and head
+svmPred <- predict(svmOutput, tesTData, type="votes")
+#Reviewing contents of svmPred using str and head
 
 #Creating a confusion matrix with second row of svmPred to the contents of testData$happy variable
 
-confMat<-data.frame(svmPred[1,],tesTData$Satisfaction)
+confMat<-data.frame(svmPred[1,],testData$Satisfaction)
 confTab<-table(confMat)
 confTab
