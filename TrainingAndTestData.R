9d0f74a952839a38b48016c5e038aa1afc43e364
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-07 12:26:56 -0500

    removing TrainingAndTestData.R

--- TrainingAndTestData.R
+++
@@ -1,7 +0,0 @@
-
-randIndex<-  sample(1:dim(satisfactionSurveyF2)[1])
-cutPoint2_3 <- floor(2*dim(satisfactionSurveyF2)[1]/3)
-trainData<- satisfactionSurveyF2[randIndex[1:cutPoint2_3],]
-trainData
-
-
