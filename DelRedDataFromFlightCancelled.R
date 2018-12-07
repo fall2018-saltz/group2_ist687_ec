415a692ceb489af8025ae0000aaa5d485d41d609
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-07 14:15:49 -0500

    Saving DelRedDataFromFlightCancelled.R

--- DelRedDataFromFlightCancelled.R
+++ DelRedDataFromFlightCancelled.R
@@ -1,4 +1,4 @@
 
 ss<-which((is.na(colSatSurvey$Flight.time.in.minutes)&colSatSurvey$Flight.cancelled=="No"))
-colSatSurvey<-colSatSurvey[-ss,]
-colSatSurvey5 <- colSatSurvey
+colSatSurvey5<-colSatSurvey[-ss,]
+#colSatSurvey5 <- colSatSurvey
