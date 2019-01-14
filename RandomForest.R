a98f624a1d850b1df50742d8af4bee0f9761513f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2019-01-14 09:02:58 -0500

    Saving RandomForest.R

--- RandomForest.R
+++ RandomForest.R
@@ -12,7 +12,7 @@ rf<-randomForest(Satisfaction~Age+Airline.Status+Gender+
                    +Flight.cancelled,data=trainData1,mtry=6,importance=TRUE,ntree=500)
 
 #?rf()
-rf
+#rf
 
 rfTrainPrediction<-predict(rf,trainData1)
 rfTrainConfMatrix<-confusionMatrix(rfTrainPrediction,trainData1$Satisfaction)
@@ -20,7 +20,7 @@ rfTrainConfMatrix<-confusionMatrix(rfTrainPrediction,trainData1$Satisfaction)
 
 rfTestPrediction<-predict(rf,testData1)
 rfTestConfMatrix<-confusionMatrix(rfTestPrediction,testData1$Satisfaction)
-rfTestConfMatrix
+#rfTestConfMatrix
 
 #rfTestPredictionprob<-predict(rf,testData, type="prob")[,2]
 #roc(as.numeric(testData$Satisfaction), as.numeric(as.matrix(rfTestPredictionprob)))$auc
